61b2070e51f521e03217007ad909f27bd923ff98
victor <vhlongon@gmail.com>
2021-02-14 20:06:48 +0100

    add button to save user

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,12 +4,13 @@ import { UserInterface, UserData } from './../models/User';
 export const UserForm = (parent: HTMLElement | null, model: UserInterface) => {
   const template = (): string => `
   <div>
-  <h1>User form</h1>
-  <div>USer name: ${model.data.name}</div>
-  <div>USer age: ${model.data.age}</div>
-  <input />
-  <button class="set-name">Change name</button>
-  <button class="set-age">Set random age</button>
+    <h1>User form</h1>
+    <div>USer name: ${model.data.name}</div>
+    <div>USer age: ${model.data.age}</div>
+    <input placeholder="${model.data.name}" />
+    <button class="set-name">Change name</button>
+    <button class="set-age">Set random age</button>
+    <button class="save-model">Save</button>
   </div>`;
 
   const onSetAgeClick = (): void => {
@@ -21,9 +22,14 @@ export const UserForm = (parent: HTMLElement | null, model: UserInterface) => {
     model.set({ name: input?.value });
   };
 
+  const onSaveClick = (): void => {
+    model.save();
+  };
+
   const eventsMap = (): { [key: string]: () => void } => ({
     'click:.set-age': onSetAgeClick,
     'click:.set-name': onSetNameClick,
+    'click:.save-model': onSaveClick,
   });
 
   return View<UserInterface, UserData>(parent, model, template, eventsMap);
